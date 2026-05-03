9ae6f9dc2f4f65ee8b33abd7f4e56cdd7d33847c
HKrobinvai <hkrobin49@gmail.com>
2026-05-03 17:20:15 +0600

    first commit

---
+++ TypeScript-learning/src/nonPrimitive.ts
@@ -0,0 +1,9 @@
+let mixArr :(string | number)[] = ["eggs",20]
+
+
+mixArr.push(true)
+
+
+let coordination : [string , string]= ["husband", "wife"] 
+
+let nextCourse : [string ,number] = ["next", 7]
---
+++ TypeScript-learning/src/primitive.ts
@@ -0,0 +1,13 @@
+let userName : string = 'next'
+
+let userId = 123;
+
+userId.toFixed()
+
+let admin : boolean = false;
+
+admin = true;
+
+let x : undefined = undefined;
+
+
